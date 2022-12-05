9424ed6fed7af4ea10eb2a3e3a02ee447f26e6cd
KILLUA <hassanidhmmou@gmail.com>
2022-12-05 19:45:16 +0100

    pipex

--- bonus_src/utils_main_bonus.c
+++ bonus_src/utils_main_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils_main_bonus_.c                                :+:      :+:    :+:   */
+/*   utils_main_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hidhmmou <hidhmmou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 21:02:04 by hidhmmou          #+#    #+#             */
-/*   Updated: 2022/12/04 22:54:59 by hidhmmou         ###   ########.fr       */
+/*   Updated: 2022/12/05 19:44:57 by hidhmmou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,4 +65,14 @@ char	*ft_strjoin(char *s1, char *s2)
 		str[len_1++] = s2[i++];
 	str[len_1] = 0;
 	return (str);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
